481c6c7f5e1721e277ed5761123a197dff82e149
Ahmed Raza <ahmedaustin03@gmail.com>
2026-03-03 19:50:04 +0500

    Added remaining Race images

--- Frontend/dashboard/src/utils/raceImages.js
+++ Frontend/dashboard/src/utils/raceImages.js
@@ -1,17 +1,53 @@
 import bahrain from "../assets/Races/Bahrain.jpg";
 import china from "../assets/Races/China.webp";
 import miami from "../assets/Races/miami.avif";
-import silverstone from "../assets/Races/silverstone.JPG";
+import silverstone from "../assets/Races/silverstone.webp";
 import suzuka from "../assets/Races/Suzuka.jpg";
 import australia from "../assets/Races/Australia.avif";
-
+import abudhabi from "../assets/Races/Abu Dhabi.avif";
+import austria from "../assets/Races/Austria.jpg";
+import Azerbijan from "../assets/Races/Azerbijan.avif";
+import belgium from "../assets/Races/Belgium.avif";
+import brazil from "../assets/Races/brazil.png";
+import canada from "../assets/Races/Canada.avif";
+import cota from "../assets/Races/COTA.webp";
+import hungary from "../assets/Races/Hungary.jpg";
+import itlay from "../assets/Races/Itlay.avif";
+import japan from "../assets/Races/Japan.avif";
+import lasvegas from "../assets/Races/Las Vegas.png";
+import madrid from "../assets/Races/madrid.webp";
+import mexico from "../assets/Races/Mexico.jpg";
+import monaco from "../assets/Races/monaco.jpg";
+import neatherlands from "../assets/Races/Neatherlands.avif";
+import qatar from "../assets/Races/Qatar.webp";
+import singapore from "../assets/Races/Singapore.avif";
+import spain from "../assets/Races/Spain.avif";
 const raceImages = {
     "bahrain" : bahrain,
     "china" : china,
     "miami" : miami,
     "silverstone" : silverstone,
     "suzuka" : suzuka,
-    "australia" : australia
+    "australia" : australia,
+    "abudhabi" : abudhabi,
+    "austria" : austria,
+    "azerbijan" : Azerbijan,
+    "belgium" : belgium,
+    "brazil" : brazil,
+    "canada" : canada,
+    "cota" : cota,
+    "hungary" : hungary,
+    "itlay" : itlay,
+    "japan" : japan,
+    "lasvegas" : lasvegas,
+    "madrid" : madrid,
+    "mexico" : mexico,
+    "monaco" : monaco,
+    "neatherlands" : neatherlands,
+    "qatar" : qatar,
+    "singapore" : singapore,
+    "spain" : spain
+
 }
 
 export default raceImages;

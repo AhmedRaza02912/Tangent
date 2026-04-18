f8e26a49c645f44e66c93a012b4daffcf4b95f81
Ahmed Raza <ahmedaustin03@gmail.com>
2026-04-18 16:09:52 +0500

    Add GA

--- Frontend/dashboard/src/main.jsx
+++ Frontend/dashboard/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
+import ReactGA from 'react-ga4'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+ReactGA.initialize('G-0Z5YXQL61Q');
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
     <App />
   </StrictMode>,
